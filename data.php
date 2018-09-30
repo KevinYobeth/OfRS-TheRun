94d4d9aed958f66c58d5f218dd3b4fce64ff3482
KevinYobeth <leokeviny@gmail.com>
2018-09-30 14:47:12 +0700

    First Commit

---
+++ data.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>OfRS | Registration</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" href="images/favicon.png" type="image/png" sizes="32x32">
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+        <!-- START OF LOADING DATA -->
+        <header id="header">
+        <div id="wrapper">
+        <div class="content">
+        <div class="inner"></div>
+		<article id="datacenter">
+
+		<center><h2 class="major">OfRS Data Center</h2></center>
+
+            <div class="field">
+		    <label for="fullname">Full Name</label>
+            <input type="text" name="fullname" id="fullname" required/>
+            </div>
+            
+        </article>
+        </div>
+        </div>
+        </div>
+        </header>
+        <!-- END OF LOADING DATA -->
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
+
---
+++ submit.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>OfRS | Thankyou!</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="icon" href="images/favicon.png" type="image/png" sizes="32x32">
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="content">
+							<div class="inner">
+								<h2>Thank You For Registering,</h2>
+								<p><?php echo $_POST['fullname']?></p><br>			
+								<?php $total = rand(1,999)?>
+								<h3>Please Kindly Transfer<br></h3><h2>IDR 200.<?php echo $total?>,-</h2><p> to BCA <b>60 411 267 25</b> a/n Stephanus Suryawan</p>
+								</div>
+									<ul class="actions stacked">
+										<li><a href="index.html" class="button primary">Back To Registration</a></li>
+									</ul>
+						</div>
+					</header>
+
+				<!-- Footer -->
+                <footer id="footer">
+						<p class="copyright">&copy; 2018 <a href = "https://instagram.com/kevinyobeth" >KevinYobeth</a>:<b>(OfRS)</b></p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
